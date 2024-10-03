5b0a8c4a14dc87c0c7e964e793e462b3c3955da4
Rofiqul Umma <70629894+Rofiq126@users.noreply.github.com>
2024-10-03 14:09:01 +0700

    init commit

---
+++ android/app/src/main/kotlin/com/example/simple_grocery_product_app/MainActivity.kt
@@ -0,0 +1,5 @@
+package com.example.simple_grocery_product_app
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity()
---
+++ lib/features/data/grocery_data.dart
@@ -0,0 +1,84 @@
+class GroceryProduct {
+  static List<Map<String, dynamic>> groceryProduct = [
+    {
+      "id": 1,
+      "name": "Apple",
+      "description": "Fresh and juicy red apples.",
+      "price": 2.99,
+      "imageUrl":
+          "https://i.pinimg.com/236x/4a/de/37/4ade3729109e48e14e0e3126f49df099.jpg"
+    },
+    {
+      "id": 2,
+      "name": "Banana",
+      "description": "Ripe bananas, perfect for snacks or smoothies.",
+      "price": 1.29,
+      "imageUrl":
+          "https://i.pinimg.com/564x/1c/16/62/1c1662f546cc85a1d77732c840ff9113.jpg"
+    },
+    {
+      "id": 3,
+      "name": "Carrot",
+      "description": "Crunchy organic carrots, rich in beta carotene.",
+      "price": 1.49,
+      "imageUrl":
+          "https://i.pinimg.com/236x/05/93/ca/0593cab4c6f6209a783b682287a2bfc6.jpg"
+    },
+    {
+      "id": 4,
+      "name": "Broccoli",
+      "description": "Fresh broccoli florets, full of vitamins.",
+      "price": 3.99,
+      "imageUrl":
+          "https://i.pinimg.com/236x/29/b3/cb/29b3cbe08421127f03eb643250590c00.jpg"
+    },
+    {
+      "id": 5,
+      "name": "Chicken Breast",
+      "description": "Boneless, skinless chicken breast, ready to cook.",
+      "price": 7.99,
+      "imageUrl":
+          "https://i.pinimg.com/236x/5f/5b/a4/5f5ba4accde536e45908af90297e8d4f.jpg"
+    },
+    {
+      "id": 6,
+      "name": "Milk",
+      "description": "1 gallon of fresh whole milk.",
+      "price": 3.49,
+      "imageUrl":
+          "https://i.pinimg.com/236x/00/00/36/0000367b81c3293883265a80963b760f.jpg"
+    },
+    {
+      "id": 7,
+      "name": "Bread",
+      "description": "Whole grain bread, perfect for sandwiches.",
+      "price": 2.79,
+      "imageUrl":
+          "https://i.pinimg.com/236x/05/29/b0/0529b0b80c3b51286d7ee62e2fd4d025.jpg"
+    },
+    {
+      "id": 8,
+      "name": "Eggs",
+      "description": "Dozen of large, farm-fresh eggs.",
+      "price": 2.99,
+      "imageUrl":
+          "https://i.pinimg.com/236x/5f/c8/fe/5fc8fedd1b3d804f4cc9f376a79298a1.jpg"
+    },
+    {
+      "id": 9,
+      "name": "Orange Juice",
+      "description": "Freshly squeezed orange juice, no sugar added.",
+      "price": 4.49,
+      "imageUrl":
+          "https://i.pinimg.com/236x/2d/77/4e/2d774e414042779b486e607e91cdf52e.jpg"
+    },
+    {
+      "id": 10,
+      "name": "Rice",
+      "description": "Long grain white rice, 5 lb bag.",
+      "price": 8.99,
+      "imageUrl":
+          "https://i.pinimg.com/236x/f9/d8/89/f9d889d0c767dc9e74e66aaa11353306.jpg"
+    }
+  ];
+}
